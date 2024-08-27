eabc9d48f38ac08b98451af74336d0edee949b58
Nathacha Cardoso <nathachaalexsandra@gmail.com>
2024-08-27 13:57:20 -0300

    add class

--- curso/src/application/Program.java
+++ curso/src/application/Program.java
@@ -2,29 +2,34 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangulo;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		
+		Triangulo x, y;
+		x = new Triangulo();
+		y = new Triangulo();
 		
 		System.out.println("Insira os medidas do triangulo X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Insira as medidas do triangulo Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2;
-		double areaTrianguloX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2;
+		double areaTrianguloX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = (yA + yB + yC) / 2;
-		double areaTrianguloY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.b + y.b + y.c) / 2;
+		double areaTrianguloY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		
 		System.out.printf("Area do triangulo X: %.4f%n", areaTrianguloX);
 		System.out.printf("Area do triangulo Y: %.4f%n", areaTrianguloY);
